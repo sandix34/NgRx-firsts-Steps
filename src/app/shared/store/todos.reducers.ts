921e12acf31b7417ddd229c9debead1e9f6e0c97
sandrine <sandrinezocca@gmail.com>
2020-06-15 16:18:22 +0200

    state changes corresponding to each action

--- src/app/shared/store/todos.reducers.ts
+++ src/app/shared/store/todos.reducers.ts
@@ -1,5 +1,6 @@
-import { Action } from '@ngrx/store';
+
 import { Todo } from '../models/todo.model';
+import * as todosAction from './todos.actions';
 
 // état constitué d'un tableau de todos
 export interface TodoState {
@@ -17,9 +18,29 @@ const initialState = {
 }; 
 
 // fonction reducer qui prend l'état de l'application, effectue des modifications dessus en fonction des actions et retourne un nouvel objet qui est le nouvel état de l'application
-export function todosReducer( state : TodoState = initialState, action: Action ) : TodoState {
-  console.log(state);
-  console.log(action); 
-  return state;
+export function todosReducer( state: TodoState = initialState, action: todosAction.TodosActionType ): TodoState {
+  switch (action.type)  {
+    case todosAction.TODO_CREATE :
+      return {
+        ...state,
+        datas: [...state.datas, action.payload]
+      };
+    case todosAction.TODO_DELETE :
+      return {
+        ...state,
+        datas: state.datas.filter( (t, i) => i !== action.payload )
+      };
+    case todosAction.TODO_TOGGLE :
+      return {
+        ...state,
+        datas: [
+          ...state.datas.map((t, i) =>
+            i === action.payload ? { ...t, done: !t.done } : t
+          )
+        ]
+      };
+    default:
+      return state;
+  }
 }
 
