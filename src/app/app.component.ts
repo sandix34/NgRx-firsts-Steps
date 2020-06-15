3477921f5375a9653edd04c1f46eb0885b2d3123
sandrine <sandrinezocca@gmail.com>
2020-06-15 16:33:49 +0200

    collect the todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from './shared/models/todo.model';
-import { TodoService } from './shared/services/todo.service';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { State } from './shared/store';
 // import de toutes les actions avec l'alias todosActions
 import * as todosAction from './shared/store/todos.actions'
+import { map } from 'rxjs/operators';
+import { TodoState } from './shared/store/todos.reducers'
 
 @Component({
   selector: 'app-root',
@@ -13,24 +14,24 @@ import * as todosAction from './shared/store/todos.actions'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos$: Observable<Todo[]> = this.todoService.todos$.asObservable();
+  public todos$: Observable<Todo[]> = this.store.pipe(
+    select('todos'),
+    map( (todoState: TodoState) => todoState.datas)
+  );
   public message: string;
 
-  constructor(private todoService: TodoService, private store: Store<State>) {}
+  constructor(private store: Store<State>) {}
 
   public addTodo() {
-    // this.todoService.addTodo({ message: this.message, done: false });
     this.store.dispatch(new todosAction.CreateTodo({ message: this.message, done: false }));
   }
 
   public toggleTodo(index: number) {
-    //this.todoService.toggleTodo(index);
     this.store.dispatch(new todosAction.ToggleTodo(index));
   }
 
   public deleteTodo(index: number) {
     console.log(index);
-    //this.todoService.deleteTodo(index);
     this.store.dispatch(new todosAction.DeleteTodo(index));
   }
 }
