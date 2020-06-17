6a44dae80a045bb41c4d30ec8795596b468db447
sandrine <sandrinezocca@gmail.com>
2020-06-17 15:33:08 +0200

    simulate an asynchronous request

---
+++ src/app/shared/services/todo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { Observable, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class TodoService {
+
+  constructor() { }
+
+  // simulation d'une requête asynchrone en utilisant l'opérateur timer
+  public getTodos(): Observable<Todo[]> {
+    return timer(2000).pipe(
+      map( () => [
+        {
+          message: 'Travailler',
+          done: false
+        }
+      ])
+    )
+  }
+}
