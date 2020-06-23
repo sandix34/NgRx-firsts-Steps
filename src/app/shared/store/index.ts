ddb9061c8e90b28f74a50e9e20287f7453c0ae15
sandrine <sandrinezocca@gmail.com>
2020-06-23 17:39:09 +0200

    add the reducer made available by the router-store library to the store

--- src/app/shared/store/index.ts
+++ src/app/shared/store/index.ts
@@ -1,13 +1,16 @@
 import * as todosReducer from './todos.reducers';
 import { ActionReducerMap } from '@ngrx/store';
+import * as routerReducer from '@ngrx/router-store';
 
 
 // état général de l'application dans le State qui est un objet contenant des todos qui ont elles-mêmes un état, TodoState
 export interface State {
   todos: todosReducer.TodoState;
+  router: routerReducer.RouterReducerState;
 }
 
 // objet reducers qui contient toutes les fonctions reducers que l'ont passe au store
 export const reducers: ActionReducerMap<State> = {
-  todos: todosReducer.todosReducer
+  todos: todosReducer.todosReducer,
+  router: routerReducer.routerReducer
 };
